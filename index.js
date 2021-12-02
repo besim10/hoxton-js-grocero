8f7518bacfc3ce9cd4d6ddea470c94fdcf33bb1c
Besim <besimsokoli10@gmail.com>
2021-12-02 12:58:01 +0100

    Finished!

--- index.js
+++ index.js
@@ -9,17 +9,19 @@ This is how an item object should look like
 }
 
 */
-const storeItemList = document.querySelector('.store--item-list')
-const cartItemList = document.querySelector('.item-list.cart--item-list')
+
+const storeItemList = document.querySelector('.store--item-list') // finding ul inside header section
+const cartItemList = document.querySelector('.item-list.cart--item-list') // finding ul inside main section
 
 
+//state object with items inside, every item is a object inside array
 const state = {
   items:[
     {
       id: '001',
       name: 'beetroot',
       price: 0.35,
-      inCart: 0
+      inCart: 0 // by default the app starts with 0 inCart or quantity
     },
     {
       id: '002',
@@ -77,13 +79,36 @@ const state = {
     }
   ]
 }
+
+// function that we send a single element inside array and it increase quantity by one
 function inCreaseQuantity(item){
   item.inCart += 1
 }
-function getInCart(item){
+// function that we send a single element inside array and it decrease quantity by one
+function deCreaseQuantity(item){
+  item.inCart -= 1
+}
+//function that accept one argument of single element and return the number of inCart inside object in the state
+function getNumberOfItemsInCart(item){
   return item.inCart
 }
 
+//function that get the one single price
+function getPrice(item){
+  return item.price
+}
+
+function showPrice(){
+  const spanEl = document.querySelector('.total-number')
+  let totalPrice = 0
+  spanEl.innerHTML =''
+  for(const item of state.items){
+    totalPrice += (getNumberOfItemsInCart(item) * getPrice(item))
+  }
+  spanEl.textContent = `£${totalPrice.toFixed(2)}`
+}
+
+// first created single item inside ul store in the header
 function storeItem(item){
   const listItem = document.createElement('li')
   const itemIcon = document.createElement('div')
@@ -97,12 +122,15 @@ function storeItem(item){
   storeItemList.append(listItem)
   listItem.append(itemIcon,listItemButton)
   itemIcon.append(imgEl)
-
+  
+  //listen to the button when the user click
   listItemButton.addEventListener('click',function(){
-    inCreaseQuantity(item)
-    renderCartItem()
+    inCreaseQuantity(item) //updating quantity by adding +1 in the states
+    render() //calling render function
   })
 }
+
+//With for loop creating all the store items, like apple bananas etc.
 function renderStoreItemList(){
   for(const item of state.items){
     storeItem(item)
@@ -110,11 +138,15 @@ function renderStoreItemList(){
 }
 renderStoreItemList()
 
+
+//render CartItem inside main
 function renderCartItem(){
 
-  cartItemList.innerHTML = ''
-  for(const item of state.items){
-    if(getInCart(item) > 0){
+  cartItemList.innerHTML = '' // after updating some states clearing the ul content
+
+  for(const item of state.items){ // looping for every item in the state object
+
+    if(getNumberOfItemsInCart(item) > 0){ // if inCart is bigger than 0 create that single element in the cartItem li inside ul
       const cartLiEl = document.createElement('li')
       const imageEl = document.createElement('img')
       imageEl.setAttribute('class','cart--item-icon')
@@ -122,23 +154,35 @@ function renderCartItem(){
       imageEl.setAttribute('alt',`${item.name}`)
       const pEl = document.createElement('p')
       pEl.textContent = `${item.name}`
+
       const decreaseButton = document.createElement('button')
       decreaseButton.classList.add('quantity-btn','remove-btn','center');
-      
       decreaseButton.textContent = '-'
+      //listen the decrease button, whena user click it update the quantity of single object in state and than call the render
+      decreaseButton.addEventListener('click',function(){
+        deCreaseQuantity(item)
+        render()
+      })
       const spanEl = document.createElement('span')
       spanEl.classList.add('quantity-text','center')
       spanEl.textContent = `${item.inCart}`
+
       const increaseButton = document.createElement('button')
       increaseButton.classList.add('quantity-btn','add-btn','center')
       increaseButton.textContent = '+'
-      
+      //listen the increase button, whena user click it update the quantity of single object in state and than call the render
+      increaseButton.addEventListener('click',function(){
+        inCreaseQuantity(item)
+        render()
+      })
       cartItemList.append(cartLiEl)
       cartLiEl.append(imageEl,pEl,decreaseButton,spanEl,increaseButton)
     } 
   }
 }
+
 function render(){
   renderCartItem()
+  showPrice()
 }
-render()
+render()//calling render function
